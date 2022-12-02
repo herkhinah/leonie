cded051c63f80b2b339954474d1c8db1197d4c55
Shekhinah Memmel <she@khinah.xyz>
2022-12-02 15:03:25 +0100

    devenv: generate rust-analyzer vscode config in shell hook

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,32 @@
 { pkgs, inputs, lib, ... }:
 let
-  fenix = inputs.fenix.packages.${pkgs.system}.stable;
-in {
-  # https://devenv.sh/pre-commit-hooks/
+  fenix = inputs.fenix.packages.${pkgs.system};
+  toolchain = fenix.stable;
+in 
+{
+  packages = [ toolchain.toolchain fenix.rust-analyzer ];
+
+  enterShell = ''
+    vscode-settings
+  '';
+
+  scripts.vscode-settings.exec = ''
+    mkdir -p .vscode
+    touch -a .vscode/settings.json
+
+    cat <<EOF > .vscode/settings.json
+    {
+      "rust-analyzer.cargo.sysroot": "${toolchain.toolchain}",
+      "rust-analyzer.server.path": "${fenix.rust-analyzer}/bin/rust-analyzer",
+    }
+    EOF
+  '';
+
   pre-commit = {
     tools = {
-      cargo = lib.mkForce fenix.cargo;
-      rustfmt = lib.mkForce fenix.rustfmt;
-      clippy = lib.mkForce fenix.clippy;
+      cargo = lib.mkForce toolchain.cargo;
+      rustfmt = lib.mkForce toolchain.rustfmt;
+      clippy = lib.mkForce toolchain.clippy;
     };
     hooks = {
       clippy.enable = true;
